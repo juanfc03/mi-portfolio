699b0309e886c42e2f6555b033c8bdab745eb604
Juan Fernández Ceacero <juanf.ceacero@gmail.com>
2026-06-03 19:34:04 +0200

    chore: configurar tailwind, fuentes y dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrains-mono",
+      provider: fontProviders.fontsource(),
+      weights: [400, 500, 700],
+      styles: ["normal"],
+      subsets: ["latin"],
+      fallbacks: ["monospace"],
+    },
+    {
+      name: "Material Symbols Outlined",
+      cssVariable: "--font-material-symbols",
+      provider: fontProviders.googleicons(),
+    },
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
